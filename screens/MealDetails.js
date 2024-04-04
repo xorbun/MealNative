b7e820059931013ece8cc984dad1e625def15942
Bruno Capobianco <bruno.capobianco@outlook.it>
2024-04-04 14:18:39 +0200

    Update MealDetails.js

--- screens/MealDetails.js
+++ screens/MealDetails.js
@@ -1,18 +1,26 @@
-import { Text,Image, View,StyleSheet,Platform } from "react-native"
+import { Text,Image, View,StyleSheet,Platform,ScrollView } from "react-native"
 const MealDetails=({route})=>{
     const title=route.params.title
     const image=route.params.image
     const ingredients=route.params.ingredients
     const steps=route.params.steps
     return(
+        <ScrollView>
         <View style={styles.maelContainer}> 
             <View style={styles.card}>
                 <Text style={styles.title}>{title}</Text>
                 <Image style={styles.image} source={{ uri: image }} />
-                <Text style={styles.ingredients}>{ingredients}</Text>
-                <Text style={styles.ingredients}>{steps}</Text>
+                <Text style={styles.title}>INGREDIENTS</Text>
+                {ingredients.map((ingred)=>(
+                    <Text style={styles.ingredients} key={ingred}>{ingred}</Text>
+                ))}
+                <Text style={styles.title}>STEPS</Text>
+                {steps.map((step)=>(
+                    <Text style={styles.steps} key={step}>-{step}</Text>
+                ))}
             </View>
         </View>
+        </ScrollView>
     )
 }
 export default MealDetails
@@ -25,19 +33,24 @@ const styles=StyleSheet.create({
         textShadowRadius: 8,
         overflow: Platform.OS === "android" ? "hidden" : "visible",
         marginHorizontal: 28,
-       
+        marginTop:12,
         backgroundColor: "white",
       },
       card:{
-        alignItems:'center',
         padding:12,
       },
       title:{
         fontSize:20,
-        fontWeight:'bold'
+        fontWeight:'bold',
+        textAlign:'center'
       },
       ingredients:{
-        fontSize:15
+        fontSize:15,
+        textAlign:'center'
+      },
+      steps:{
+        marginBottom:2,
+        textAlign:'left'
       },
     image: {
         width: "100%",
